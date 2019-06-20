0e89b8db160117099c5df0501e268516e6d8ca16
Hitalo Siqueira <hitalo.siqueira@gympass.com>
2019-06-20 18:21:06 -0300

    Values Variabbles Types

---
+++ src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,25 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+
+  //VALUES
+  //Values are IMMUTABLE
+  val x: Int = 42
+  val aString: String = "Hello"
+  val aBoolean: Boolean = true
+  val aChar: Char = 'a'
+  val anInt: Int = x
+  val aShort: Short = 2
+  val aLong: Long = 234L
+  val aFloat: Float = 2.5f
+  val aDouble: Double = 2.5
+
+  //==========================||==============================//
+
+  //VARIABLES
+  //Variables are MUTABLE
+
+  var y: Int = 9
+  y = 10
+
+}
